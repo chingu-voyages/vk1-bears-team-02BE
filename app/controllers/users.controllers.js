bf119ae9306f926c217e47183aabf7a141ee22f6
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2020-12-19 01:10:54 +0800

    fix httpmessage

--- app/controllers/users.controllers.js
+++ app/controllers/users.controllers.js
@@ -17,10 +17,10 @@ exports.create = async (req, res) => {
 		});
 		try {
 			const data = await user.save();
-			res.status(httpsStatus.CREATED).json({
+			res.status(httpsStatus.OK).json({
 				data: data,
 				message: "user has been added",
-				status: httpsStatus.CREATED,
+				status: httpsStatus.OK,
 			});
 		} catch (error) {
 			res.status(httpsStatus.INTERNAL_SERVER_ERROR).json({
@@ -99,10 +99,10 @@ exports.update = async (req, res) => {
 					status: httpsStatus.NOT_FOUND,
 				});
 			}
-			return res.status(httpsStatus.CREATED).send({
+			return res.status(httpsStatus.OK).send({
 				data: data,
 				message: "record updated for user id " + req.params.userId,
-				status: httpsStatus.CREATED,
+				status: httpsStatus.OK,
 			});
 		} catch (error) {
 			res.status(httpsStatus.INTERNAL_SERVER_ERROR).send({
